2c9018197afb83e14dfc51bcdcabd8b53d61374e
Banan-dot <semke.danya@mail.ru>
2023-05-28 21:02:42 +0500

    Refactor

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,23 +1,40 @@
 import { Sequelize } from "sequelize";
 
-const { DB_URL } = process.env;
+const { DB_NAME, DB_PORT, DB_USER, DB_URL, DB_PASSWORD, DB_HOST } = process.env;
 
 if (!DB_URL) {
+  throw new Error(
+      "Отсутствует название базы данных. Введите в .env DB_NAME=`Ваше название`"
+  );
+}
+
+if (!DB_NAME) {
   throw new Error(
     "Отсутствует название базы данных. Введите в .env DB_NAME=`Ваше название`"
   );
 }
 
-// if (!DB_USER) {
-//   throw new Error(
-//     "Отсутствует имя пользователя. Введите в .env DB_USER=`Имя пользователя`"
-//   );
-// }
-//
-// if (!Number(DB_PORT)) {
-//   throw new Error(
-//     "Порт базы данных не является числом. Введите в .env DB_PORT=`Числовое значение`"
-//   );
-// }
+if (!DB_USER) {
+  throw new Error(
+    "Отсутствует имя пользователя. Введите в .env DB_USER=`Имя пользователя`"
+  );
+}
 
-export const db = new Sequelize(DB_URL);
+if (!Number(DB_PORT)) {
+  throw new Error(
+    "Порт базы данных не является числом. Введите в .env DB_PORT=`Числовое значение`"
+  );
+}
+
+let sequelize;
+
+if (DB_URL) {
+  sequelize = new Sequelize(DB_URL);
+} else {
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    dialect: "postgres",
+    host: DB_HOST,
+    port: Number(DB_PORT),
+  });
+}
+export const db = sequelize;
